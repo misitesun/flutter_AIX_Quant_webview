c75ef944f78678b62b61122118bdbeabf5dab34b
sly <934353403@qq.com>
2026-09-11 18:20:18 +0800

    该项目已废弃，不在打包维护

--- lib/common/values/constants.dart
+++ lib/common/values/constants.dart
@@ -1,7 +1,7 @@
 /// 常量
 class Constants {
   // 服务 api
-  static const wpApiBaseUrl = 'https://www.xsmartpay.com';
+  static const wpApiBaseUrl = 'https://www.aixquant.net';
   // 服务 socket
   static const wpSocketUrl = 'wss://happyxfb.vip/ws1';
 
@@ -9,6 +9,6 @@ class Constants {
   static const storageLanguageCode = 'language_code';
 
   // h5Url
-  static const h5Url = 'http://192.168.110.102:8080/aix/';
+  static const h5Url = 'https://www.aixquant.net/aix/';
   static const xspayUrl = 'https://www.xsmartpay.com';
 }
